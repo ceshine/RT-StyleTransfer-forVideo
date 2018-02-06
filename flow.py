90cd91001a7692b8ce7413e8e52f2e010aaeea69
curaai <curaai00@gmail.com>
2018-02-06 13:43:29 +0900

    optical flow with opencv
    I don't know this is right way, but I can't do Deepmatch & Deepflow

--- flow.py
+++
@@ -1,9 +0,0 @@
-with open('flow.flo', 'rb') as f:
-    a = f.readline()
-    print(len(a))
-    tag = a[:4]
-    width = a[4:8]
-    height = a[8:12]
-    data = a[12:]
-    print(int(width, 16))
-    pass
